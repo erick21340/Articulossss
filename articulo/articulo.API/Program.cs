e2449a45073253aa03b1255a3507d49da3917af1
Erick20247 <Torot0086@gmail.vom>
2024-06-24 14:14:05 -0400

    el modificando el nav menu

---
+++ articulo/articulo.API/Program.cs
@@ -0,0 +1,41 @@
+using articulo.API.DAL;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+
+var ConStr = builder.Configuration.GetConnectionString("ConStr");
+builder.Services.AddDbContextFactory<Contexto>(o => o.UseSqlite(ConStr));
+
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+
+
+    app.UseSwagger();
+    app.UseSwaggerUI();
+
+
+app.UseCors(options =>
+{
+    options.AllowAnyOrigin();
+    options.AllowAnyHeader(); 
+    options.AllowAnyMethod(); 
+});
+
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
