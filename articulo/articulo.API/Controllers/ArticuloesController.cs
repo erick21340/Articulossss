cf7ebe6c6e9add6426887a9dadfb433e4d48d377
Erick20247 <Torot0086@gmail.vom>
2024-06-24 14:09:05 -0400

    eh creado los controladores

---
+++ articulo/articulo.API/Controllers/ArticuloesController.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Compartido.Models;
+using articulo.API.DAL;
+
+namespace articulo.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ArticuloesController : ControllerBase
+    {
+        private readonly Contexto _context;
+
+        public ArticuloesController(Contexto context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Articuloes
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Articulo>>> GetArticulos()
+        {
+            return await _context.Articulos.ToListAsync();
+        }
+
+        // GET: api/Articuloes/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Articulo>> GetArticulo(int id)
+        {
+            var articulo = await _context.Articulos.FindAsync(id);
+
+            if (articulo == null)
+            {
+                return NotFound();
+            }
+
+            return articulo;
+        }
+
+        // PUT: api/Articuloes/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutArticulo(int id, Articulo articulo)
+        {
+            if (id != articulo.ArticuloId)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(articulo).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ArticuloExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Articuloes
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Articulo>> PostArticulo(Articulo articulo)
+        {
+            _context.Articulos.Add(articulo);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetArticulo", new { id = articulo.ArticuloId }, articulo);
+        }
+
+        // DELETE: api/Articuloes/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteArticulo(int id)
+        {
+            var articulo = await _context.Articulos.FindAsync(id);
+            if (articulo == null)
+            {
+                return NotFound();
+            }
+
+            _context.Articulos.Remove(articulo);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool ArticuloExists(int id)
+        {
+            return _context.Articulos.Any(e => e.ArticuloId == id);
+        }
+    }
+}
