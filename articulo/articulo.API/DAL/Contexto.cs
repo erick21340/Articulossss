e2ed33d1f4f4375d60d6e6f4e8410afe0edcdc52
Erick20247 <Torot0086@gmail.vom>
2024-06-24 14:10:16 -0400

    eh creado el contexto

---
+++ articulo/articulo.API/DAL/Contexto.cs
@@ -0,0 +1,13 @@
+﻿using Compartido.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace articulo.API.DAL
+{
+    public class Contexto :DbContext
+    {
+
+        public Contexto(DbContextOptions<Contexto> options) : base(options) { }
+
+        public DbSet<Articulo> Articulos { get; set; }
+    }
+}
